cb43eebfdc02e69f061e2d82112d097b68ada72e
Veeti Paananen <veeti.paananen@rojekti.fi>
2014-09-29 02:39:34 +0300

    Disable secure surface views
    Bug: fixes #2

--- src/main/java/fi/veetipaananen/android/disableflagsecure/DisableFlagSecureModule.java
+++ src/main/java/fi/veetipaananen/android/disableflagsecure/DisableFlagSecureModule.java
@@ -1,5 +1,7 @@
 package fi.veetipaananen.android.disableflagsecure;
 
+import android.os.Build;
+import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -14,7 +16,11 @@ public class DisableFlagSecureModule implements IXposedHookLoadPackage {
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         XposedHelpers.findAndHookMethod(Window.class, "setFlags", int.class, int.class,
                 mRemoveSecureFlagHook);
-    }
+        if (Build.VERSION.SDK_INT >= 17) {
+            XposedHelpers.findAndHookMethod(SurfaceView.class, "setSecure", boolean.class,
+                    mRemoveSetSecureHook);
+        }
+	}
 
     private final XC_MethodHook mRemoveSecureFlagHook = new XC_MethodHook() {
         @Override
@@ -22,6 +28,13 @@ public class DisableFlagSecureModule implements IXposedHookLoadPackage {
             Integer flags = (Integer) param.args[0];
             flags &= ~WindowManager.LayoutParams.FLAG_SECURE;
             param.args[0] = flags;
+        }
+    };
+
+    private final XC_MethodHook mRemoveSetSecureHook = new XC_MethodHook() {
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            param.args[0] = false;
         }
     };
 
